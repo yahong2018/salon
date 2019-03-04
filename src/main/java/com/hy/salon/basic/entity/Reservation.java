3525a219b3a7ac4a3b0e3c84e01af1512229bf7b
it-fenggang <17633728895@163.com>
2019-03-04 18:19:36 +0800

    新增实体

---
+++ src/main/java/com/hy/salon/basic/entity/Reservation.java
@@ -0,0 +1,18 @@
+package com.hy.salon.basic.entity;
+
+import com.zhxh.core.data.TraceableEntity;
+import com.zhxh.core.data.meta.annotation.DataTableConfiguration;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+@DataTableConfiguration("reservation")
+@Getter
+@Setter
+public class Reservation extends TraceableEntity<Long> {
+    private Long memberId ;         //会员
+    private Long stuffId ;          // 预约美容师
+    private Long roomId  ;          // 预约的房间
+    private Date timeStart ;        // 开始时间
+    private Date timeEnd ;          // 结束时间
+    private Integer recordStatus  ; // 记录状态：0.未开始  1.已确认    2.服务中    3.已完成
+}
