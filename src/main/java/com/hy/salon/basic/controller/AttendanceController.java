8d7b5135ed57ffa48d63ddefc01e3c71e72a2527
it-fenggang <17633728895@163.com>
2019-03-04 18:15:14 +0800

    签到

--- src/main/java/com/hy/salon/basic/controller/AttendanceController.java
+++ src/main/java/com/hy/salon/basic/controller/AttendanceController.java
@@ -1,10 +1,16 @@
 package com.hy.salon.basic.controller;
 
+import com.hy.salon.basic.entity.AttendanceSheet;
 import com.hy.salon.basic.service.AttendanceSheetService;
+import com.hy.salon.basic.vo.Result;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
+import java.util.Date;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/hy/basic/attendance")
@@ -31,8 +37,30 @@ public class AttendanceController {
 //
 //        write(jsonObj,resp);
 //    }
-
-
+    /**
+     * 签到
+     */
+    @ResponseBody
+    @RequestMapping(value = "SignIn",method = RequestMethod.GET)
+    public Result SignIn(Long stuffId,String address){
+        Result result=new Result();
+        AttendanceSheet condition=new AttendanceSheet();
+        condition.setStuffId(stuffId);
+        condition.setAttendanceTime(new Date());
+        condition.setAddress(address);
+        try {
+            attendanceSheetService.insert(condition);
+            result.setMsg("签到成功");
+            result.setMsgcode("0");
+            result.setSuccess(true);
+        }catch (Exception e){
+            e.printStackTrace();
+            result.setMsg("签到失败");
+            result.setMsgcode("1");
+            result.setSuccess(false);
+        }
+        return result;
+    }
 
 
 
