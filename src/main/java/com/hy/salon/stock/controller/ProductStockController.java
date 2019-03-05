a881827387bd27c9f50e2a06492cf65d3f6f364c
it-fenggang <17633728895@163.com>
2019-03-05 17:58:49 +0800

    3月5号

--- src/main/java/com/hy/salon/stock/controller/ProductStockController.java
+++ src/main/java/com/hy/salon/stock/controller/ProductStockController.java
@@ -1,11 +1,11 @@
 package com.hy.salon.stock.controller;
 
+import com.hy.salon.basic.common.StatusUtil;
 import com.hy.salon.basic.vo.Result;
 import com.hy.salon.stock.dao.ProductSockDAO;
 import com.hy.salon.stock.entity.ProductStock;
 import com.zhxh.core.data.BaseDAOWithEntity;
 import com.zhxh.core.web.SimpleCRUDController;
-import io.swagger.models.auth.In;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,5 +24,24 @@ public class ProductStockController extends SimpleCRUDController<ProductStock> {
     @Override
     protected BaseDAOWithEntity<ProductStock> getCrudDao() {
         return productSockDAO;
+    }
+    /**
+     * 按门店查询库存的情况
+     */
+    @ResponseBody
+    @RequestMapping(value = "findProductStockByWarehouseId",method = RequestMethod.GET)
+    public Result findProductStockByWarehouseId(Long warehouseId){
+        Result result=new Result();
+        try {
+            List<ProductStock>list=productSockDAO.findProductStockByWarehouseId(warehouseId);
+            result.setMsgcode(StatusUtil.OK);
+            result.setData(list);
+            result.setSuccess(true);
+        }catch (Exception e){
+            e.printStackTrace();
+            result.setMsgcode(StatusUtil.ERROR);
+            result.setSuccess(false);
+        }
+        return result;
     }
 }
