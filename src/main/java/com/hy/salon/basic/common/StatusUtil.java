ad42b6b70ecb37f7f02b2c7b7ac8edc647204037
it-fenggang <17633728895@163.com>
2019-03-05 14:56:49 +0800

    1

---
+++ src/main/java/com/hy/salon/basic/common/StatusUtil.java
@@ -0,0 +1,13 @@
+package com.hy.salon.basic.common;
+
+public class StatusUtil {
+    /**
+     * 调拨申请单
+     * 4.已完成   3.已批准   2.已驳回   1.已撤销  0.已提交(注意，一旦提交，就不可以修改，只能撤销以后重新提交)
+     */
+    public static final Integer TIJ=0;
+    public static final Integer CHEX=1;
+    public static final Integer BOH=2;
+    public static final Integer PIZ=3;
+    public static final Integer WANC=4;
+}
