ff6a1bd7c56b9b05ffc9e52feef6d17f7bdf8cfb
it-fenggang <17633728895@163.com>
2019-03-08 17:21:29 +0800

    3月8号预约相关

---
+++ src/main/java/com/hy/salon/basic/vo/StuffVo.java
@@ -0,0 +1,14 @@
+package com.hy.salon.basic.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StuffVo {
+    private Long recordId;
+    private String stuffName;//员工名字
+    private Integer reservation;//预约数量
+    private String role;//员工角色
+
+}
