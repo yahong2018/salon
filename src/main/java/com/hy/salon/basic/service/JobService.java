3d31567b857793ea21f2b196470a18694b3bdfe2
zhanfengyuanheyi <2548485782@qq.com>
2019-03-08 09:16:13 +0800

    登陆接口

---
+++ src/main/java/com/hy/salon/basic/service/JobService.java
@@ -0,0 +1,27 @@
+package com.hy.salon.basic.service;
+
+import com.hy.salon.basic.dao.JobDAO;
+import com.hy.salon.basic.dao.ProductDao;
+import com.hy.salon.basic.entity.Job;
+import com.hy.salon.basic.entity.StuffJob;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component("jobService")
+public class JobService {
+
+    @Resource(name = "jobDAO")
+    private JobDAO jobDAO;
+    public List<Job> getJobList(long stuffId) {
+        String where="stuff_id=#{stuffId}";
+        Map parameters = new HashMap();
+        parameters.put("stuffId", stuffId);
+        return jobDAO.getSqlHelper().getSqlSession().selectList(GET_JOB_BY_USERID,parameters);
+       // return jobDAO.getByWhere(where,parameters);
+    }
+    protected final static String GET_JOB_BY_USERID = "com.hy.salon.basic.dao.GET_JOB_BY_USERID";
+}
