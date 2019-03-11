1e9df435355fa050f726719fc5c425e9aace9de0
it-fenggang <17633728895@163.com>
2019-03-11 17:46:59 +0800

    3月11号考勤相关

---
+++ src/main/java/com/hy/salon/basic/dao/WorkSummaryDao.java
@@ -0,0 +1,9 @@
+package com.hy.salon.basic.dao;
+
+import com.hy.salon.basic.entity.WorkSummary;
+import com.zhxh.core.data.BaseDAOWithEntity;
+import org.springframework.stereotype.Component;
+
+@Component("workSummaryDao")
+public class WorkSummaryDao extends BaseDAOWithEntity<WorkSummary> {
+}
