8d34761c7e7d637871ead768692eb5016eb3717f
it-fenggang <17633728895@163.com>
2019-03-12 16:00:08 +0800

    3月12

--- src/main/java/com/hy/salon/basic/service/ScheduleService.java
+++ src/main/java/com/hy/salon/basic/service/ScheduleService.java
@@ -1,14 +1,11 @@
 package com.hy.salon.basic.service;
 
-import com.hy.salon.basic.dao.SalonDao;
 import com.hy.salon.basic.dao.ScheduleDao;
-import com.hy.salon.basic.dao.StuffDao;
-import com.hy.salon.basic.entity.Salon;
-import com.hy.salon.basic.entity.Stuff;
-import com.hy.salon.basic.vo.NurseLogVo;
+import com.hy.salon.basic.entity.Schedule;
 import com.hy.salon.basic.vo.ShiftVo;
 import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,5 +17,21 @@ public class ScheduleService {
 
     public List<ShiftVo> getScheduleByTime(Long recordId, String timeStart, String timeEnd) {
         return scheduleDao.getScheduleByTime(recordId,timeStart,timeEnd);
+    }
+
+    public void updateStuffSchedule(List<Schedule> list) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        for (Schedule schedule : list) {
+            //查询该员工的排班信息
+            String scheduleWhere = "stuff_id=#{stuffId} and day=#{day}";
+            Map<String, Object> scheduleParameter = new HashMap<>();
+            scheduleParameter.put("stuffId", schedule.getStuffId());
+            scheduleParameter.put("day", sdf.format(schedule.getDay()));
+            Schedule one = scheduleDao.getOne(scheduleWhere, scheduleParameter);
+            if(one!=null){
+                scheduleDao.deleteById(one.getRecordId());
+            }
+            scheduleDao.insert(schedule);
+        }
     }
 }
