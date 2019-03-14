e452e9559313340d4bbbe2ff073c682e20d5018a
it-fenggang <17633728895@163.com>
2019-03-14 18:12:24 +0800

    日志模板

--- src/main/java/com/hy/salon/basic/dao/NurseLogDao.java
+++ src/main/java/com/hy/salon/basic/dao/NurseLogDao.java
@@ -1,9 +1,24 @@
 package com.hy.salon.basic.dao;
 
 import com.hy.salon.basic.entity.NurseLog;
+import com.hy.salon.basic.entity.NurseLogModel;
 import com.zhxh.core.data.BaseDAOWithEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Component("nurseLogDao")
 public class NurseLogDao extends BaseDAOWithEntity<NurseLog> {
+    public List<NurseLogModel> getLogModel(Integer pageNo, Integer pageSize) {
+        int page=(pageNo-1)*pageSize;
+        Map parameters = new HashMap();
+        parameters.put("page", page);
+        parameters.put("pageSize", pageSize);
+        return this.getSqlHelper().getSqlSession().selectList(SQL_GET_LOG_MODEL, parameters);
+    }
+    protected final static String SQL_GET_LOG_MODEL = "com.hy.salon.basic.dao.GET_LOG_MODEL";
+
+
 }
