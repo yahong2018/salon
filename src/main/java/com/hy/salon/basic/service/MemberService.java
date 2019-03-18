bab6170dabf7e7841409237e517574ca3fa3bdca
it-fenggang <17633728895@163.com>
2019-03-18 17:55:47 +0800

    档案

---
+++ src/main/java/com/hy/salon/basic/service/MemberService.java
@@ -0,0 +1,82 @@
+package com.hy.salon.basic.service;
+
+import com.hy.salon.basic.dao.MemberDao;
+import com.hy.salon.basic.dao.SalonDao;
+import com.hy.salon.basic.dao.StuffDao;
+import com.hy.salon.basic.entity.Member;
+import com.hy.salon.basic.entity.Salon;
+import com.hy.salon.basic.entity.Stuff;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component("memberService")
+@Transactional(rollbackFor = Exception.class)
+public class MemberService {
+
+    @Resource(name = "memberDao")
+    private MemberDao memberDao;
+
+    @Resource(name = "stuffDao")
+    private StuffDao stuffDao;
+
+    @Resource(name = "salonDao")
+    private SalonDao salonDao;
+
+    public List<Member> getArchives(Long initialStoreId) {
+        List<Member> list=new ArrayList<>();
+        if(initialStoreId==0){
+            list = addAttribute(memberDao.getAll());
+        }else{
+            Map parameters = new HashMap();
+            parameters.put("initialStoreId", initialStoreId);
+            Map listMap = new HashMap();
+            String where = "initial_store_id=#{initialStoreId}";
+            listMap.put("where", where);
+            list = addAttribute(memberDao.getList(listMap, parameters));
+        }
+        return list;
+    }
+    private List<Member> addAttribute(List<Member> list){
+        for (Member member : list) {
+            String where = "record_id=#{recordId}";
+            Map parameters = new HashMap();
+            parameters.put("recordId", member.getPrimaryBeautician());
+            Stuff stuff = stuffDao.getOne(where,parameters);
+            member.setPrimaryName(stuff.getStuffName());
+        }
+        return list;
+    }
+
+    public Member getCustomerFiles(Long recordId) {
+        String where = "record_id=#{recordId}";
+        Map parameters = new HashMap();
+        parameters.put("recordId", recordId);
+        Member member = memberDao.getOne(where,parameters);
+        Map stuffParameters = new HashMap();
+        stuffParameters.put("recordId", member.getPrimaryBeautician());
+        Stuff stuff = stuffDao.getOne(where,stuffParameters);
+        Map salonParameters = new HashMap();
+        salonParameters.put("recordId", member.getInitialStoreId());
+        Salon salon = salonDao.getOne(where,salonParameters);
+        member.setPrimaryName(stuff.getStuffName());
+        member.setSalonName(salon.getSalonName());
+        return member;
+    }
+
+    public void addMember(Member member) {
+        //设置会员默认值
+        member.setMemberGradeId(3L);
+        member.setBalance(0.0);
+        member.setIntegral(0.0);
+        member.setDebt(0.0);
+        member.setAmountConsumer(0.0);
+        member.setAmountCharge(0.0);
+        memberDao.insert(member);
+    }
+}
