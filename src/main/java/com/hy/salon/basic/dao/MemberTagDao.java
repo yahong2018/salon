6bc70243175cc7e11f4b5c28a9d7abb86fd3ea7f
it-fenggang <17633728895@163.com>
2019-03-20 18:28:59 +0800

    3.20

---
+++ src/main/java/com/hy/salon/basic/dao/MemberTagDao.java
@@ -0,0 +1,9 @@
+package com.hy.salon.basic.dao;
+
+import com.hy.salon.basic.entity.MemberTag;
+import com.zhxh.core.data.BaseDAOWithEntity;
+import org.springframework.stereotype.Component;
+
+@Component("memberTagDao")
+public class MemberTagDao extends BaseDAOWithEntity<MemberTag> {
+}
