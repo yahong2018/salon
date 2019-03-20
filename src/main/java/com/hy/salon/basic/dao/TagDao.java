ea43559600429e150b62026713420e5fec8b08dd
it-fenggang <17633728895@163.com>
2019-03-20 18:29:07 +0800

    3.20

---
+++ src/main/java/com/hy/salon/basic/dao/TagDao.java
@@ -0,0 +1,10 @@
+package com.hy.salon.basic.dao;
+
+
+import com.hy.salon.basic.entity.Tag;
+import com.zhxh.core.data.BaseDAOWithEntity;
+import org.springframework.stereotype.Component;
+
+@Component("tagDao")
+public class TagDao extends BaseDAOWithEntity<Tag> {
+}
