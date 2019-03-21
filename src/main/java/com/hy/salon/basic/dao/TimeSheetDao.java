8745713af5d5c43e20917798378869100f28d538
it-fenggang <17633728895@163.com>
2019-03-21 15:10:42 +0800

    考勤新增接口

--- src/main/java/com/hy/salon/basic/dao/TimeSheetDao.java
+++ src/main/java/com/hy/salon/basic/dao/TimeSheetDao.java
@@ -6,13 +6,14 @@ import com.zhxh.core.data.BaseDAOWithEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component("timeSheetDao")
 public class TimeSheetDao extends BaseDAOWithEntity<TimeSheet> {
 
     public TimeSheet getTSheet(Long recordId,String cTime, String eTime) {
-        String where = "stuff_id=#{recordId} and time_start between #{cTime} and #{eTime}";
+        String where = "stuff_id=#{stuffId} and time_start between #{cTime} and #{eTime}";
         //String where = "stuff_id=#{recordId}";
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("stuffId", recordId);
@@ -20,12 +21,18 @@ public class TimeSheetDao extends BaseDAOWithEntity<TimeSheet> {
         parameters.put("eTime", eTime);
         return this.getOne(where,parameters);
     }
-    public TimeSheet getTSheets(Long recordId,String time) {
-        String where = "stuff_id=#{recordId} and 'day'=#{time}";
+    //查询当月员工出勤记录
+    public List<TimeSheet> getTSheets(Long recordId, String time) {
+        String timeStart=time+"00";
+        String timeEnd=time+"31";
+        String where = "stuff_id=#{stuffId} and day between  #{timeStart} and  #{timeEnd}";
+        Map map=new HashMap();
+        map.put("where",where);
         //String where = "stuff_id=#{recordId}";
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("stuffId", recordId);
-        parameters.put("time", time);
-        return this.getOne(where,parameters);
+        parameters.put("timeStart", timeStart);
+        parameters.put("timeEnd", timeEnd);
+        return this.getList(map,parameters);
     }
 }
