b82c2b17b58e6fdfcdf5d234e41c7a4d91976ae8
yahong2018 <SoftwareChina@qq.com>
2019-04-01 11:37:46 +0800

    修改LogoinFilter的逻辑，对Session过期自动跳转到登录页。

--- src/main/java/com/zhxh/admin/filters/LoginFilter.java
+++ src/main/java/com/zhxh/admin/filters/LoginFilter.java
@@ -37,15 +37,9 @@ public class LoginFilter implements Filter {
 
                 String redirectUrl = "";
                 String url = httpServletRequest.getRequestURI();
-                if(!"/".equalsIgnoreCase(url)&& !url.endsWith(".handler")){
-                    chain.doFilter(request, response);
-                    return;
-                }
-
                 if (!url.contains(SysEnv.getUrlLoginPage())) {
                     SystemUser currentLogin = authenticateService.getCurrentLogin();
-                    if(currentLogin==null /* 没有登录 */
-                            || !systemUserService.canRun(currentLogin.getRecordId(), url)/* 当前用户没有权限 */) {
+                    if(currentLogin==null) {
                         redirectUrl = SysEnv.getAppRoot() + SysEnv.getUrlLoginPage();
                     }
                 }
