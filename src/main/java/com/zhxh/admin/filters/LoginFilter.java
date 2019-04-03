a8b863a9f920fca695fc2a3e90f96c0c1d9c5425
yahong2018 <SoftwareChina@qq.com>
2019-04-03 11:20:01 +0800

    将原来的单页面应用转变为多页面应用。

--- src/main/java/com/zhxh/admin/filters/LoginFilter.java
+++ src/main/java/com/zhxh/admin/filters/LoginFilter.java
@@ -2,6 +2,7 @@ package com.zhxh.admin.filters;
 
 import com.zhxh.admin.entity.SystemUser;
 import com.zhxh.admin.service.AuthenticateService;
+import com.zhxh.admin.service.SystemProgramService;
 import com.zhxh.admin.service.SystemUserService;
 import com.zhxh.core.env.SysEnv;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -13,19 +14,36 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 @ServletComponentScan
 @WebFilter(urlPatterns = "/*")
 public class LoginFilter implements Filter {
-    @Resource(name="systemUserService")
+    @Resource(name = "systemUserService")
     private SystemUserService systemUserService;
 
-    @Resource(name="authenticateService")
+    @Resource(name = "authenticateService")
     private AuthenticateService authenticateService;
+
+    @Resource(name = "systemProgramService")
+    private SystemProgramService systemProgramService;
 
     @Override
     public void init(FilterConfig filterConfig) {
+    }
+    /**
+     * 获取链接的后缀名
+     *
+     * @return
+     */
+    private static String parseSuffix(String url) {
+        int index = url.lastIndexOf(".");
+        if(index >0){
+            return url.substring(index +1);
+        }
+        return url;
     }
 
     @Override
@@ -37,17 +55,18 @@ public class LoginFilter implements Filter {
 
                 String redirectUrl = "";
                 String url = httpServletRequest.getRequestURI();
-                /*
-                if(!"/".equalsIgnoreCase(url)&& !url.endsWith(".handler")){
+
+                String postFix = parseSuffix(url);
+                boolean isSecuredUrl = SysEnv.GetSecuredUrlPatterns().contains(postFix);
+                if (!isSecuredUrl) {
                     chain.doFilter(request, response);
                     return;
                 }
-                */
 
                 if (!url.contains(SysEnv.getUrlLoginPage())) {
                     SystemUser currentLogin = authenticateService.getCurrentLogin();
-                    if(currentLogin==null /* 没有登录 */
-                        /* || !systemUserService.canRun(currentLogin.getRecordId(), url) 当前用户没有权限 */) {
+                    if (currentLogin == null /* 没有登录 */
+                            || !systemUserService.canRun(currentLogin.getRecordId(), url) /*当前用户没有权限 */) {
                         redirectUrl = SysEnv.getAppRoot() + SysEnv.getUrlLoginPage();
                     }
                 }
