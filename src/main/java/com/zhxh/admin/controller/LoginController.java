b567fecc71a180d738e88a2bd6ca51930dca31bb
zhanfengyuanheyi <2548485782@qq.com>
2019-04-12 09:58:12 +0800

    员工列表修改，密码修改

--- src/main/java/com/zhxh/admin/controller/LoginController.java
+++ src/main/java/com/zhxh/admin/controller/LoginController.java
@@ -111,7 +111,7 @@ public class LoginController {
             result.setCode(LoginResult.LOGIN_CODE_OK);
             result.setMessage("登录成功！");
             result.setSuccess(true);
-            result.setData("");
+            result.setData(null);
 
         } catch (Exception e) {
             Logger.error(e);
